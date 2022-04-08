faf3bf966069f8aaf0f1aeb7d0dbf221ba68031b
Shon <75859175+fancyshon@users.noreply.github.com>
2022-04-08 17:47:21 +0800

    Update server.py

--- routers/server.py
+++ routers/server.py
@@ -1,37 +1,65 @@
 import sqlite3
 from fastapi import APIRouter
-from routers.LogIn import check_if_registered
-from database import SESSION
+from sklearn import tree
+# from database import SESSION
 
+def check_if_registered(id, c):
+    execution = 'SELECT id FROM user_data'
+    c.execute(execution)
+    id_list = c.fetchall()
+    target = (id,)
+
+    return target in id_list
 
 # app = fastapi.FastAPI()
 router = APIRouter()
 conn = sqlite3.connect("./UserData.db", check_same_thread=False)
 c = conn.cursor()
 
+query_sql = "SELECT * FROM sqlite_master WHERE type = 'table' AND name = ?"
+listOfTable = c.execute(query_sql, ('user_data',)).fetchall()
+if listOfTable == []:
+    c.execute('''CREATE TABLE user_data(
+        id TEXT PRIMARY KEY NOT NULL,
+        pwd TEXT NOT NULL );''')
+    conn.commit()
+
 
 # Sign up
 @router.get("/sign_up/{user_id}")
 async def send_user_data(user_id: str, user_pwd: str, comfirm_pwd: str):
     # http://127.0.0.1:8000/sign_up/Shon?user_pwd=12344&comfirm_pwd=12344
+    response={
+        'Successful':False,
+        'Comfirm_pwd_wrongly':False,
+        'Used_ID':False
+    }
+
+    if user_pwd != comfirm_pwd:
+        response['Comfirm_pwd_wrongly']=True
 
     if not check_if_registered(user_id, c):
-        sql_execution = 'INSERT INTO user_data (id, pwd) VALUES ("{}", "{}")'.format(user_id, comfirm_pwd)
+        sql_execution = 'INSERT INTO user_data (id, pwd) VALUES ("{}", "{}")'.format(user_id, user_pwd)
 
         c.execute(sql_execution)
         conn.commit()
+        response["Successful"]=True
 
-        return "Sign up successfully"
     else:
-        return "This ID has been used"
+        response['Used_ID']=True
 
+    return response
 
 @router.get("/log_in/{user_id}")
 async def log_in(user_id: str, user_pwd: str):
-    # http://127.0.0.1:8000/log_in/Shon?user_pwd=12344&comfirm_pwd=12344
+    # http://127.0.0.1:8000/log_in/Shon?user_pwd=12344
+    response={
+        'Registered':False,
+        'Successful':False,
+    }
 
     if not check_if_registered(user_id, c):
-        return "You don't have an account, please register one"
+        response['Registered']=False
     else:
 
         execution = 'SELECT pwd FROM user_data WHERE id="{}"'.format(user_id)
@@ -41,6 +69,10 @@ async def log_in(user_id: str, user_pwd: str):
         if user_pwd == pwd_in_db:
             c.close()
             conn.close()
-            return "Log in successfully"
+            response['Registered']=True
+            response['Successful']=True
         else:
-            return "Wrong password, please try again"
+            response['Registered']=True
+            response['Successful']=False
+
+    return response
